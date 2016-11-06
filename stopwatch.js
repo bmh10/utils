af04f048ac846d2d0ff8de43df1a79f6437cecb1
bmh10 <bmhomer13@gmail.com>
2016-11-06 12:34:53 +0000

    Basic stopwatch

---
+++ stopwatch.js
@@ -0,0 +1,66 @@
+var running = false;
+var startTime = -1;
+
+function initTime()
+{
+  document.getElementById('time').innerHTML = formatTime(0, 0, 0, 0);
+  tick();
+}
+
+function updateTimer()
+{
+  var btn1 = document.getElementById('b1');
+  if (running)
+  {
+    running = false;
+    btn1.innerHTML = "Start";
+    btn1.className = "btn btn-success";
+  }
+  else
+  {
+    if (startTime === -1)
+    {
+      startTime = new Date();
+    }
+    running = true;
+    btn1.innerHTML = "Stop";
+    btn1.className = "btn btn-danger";
+  }
+}
+
+function tick()
+{
+  if (running)
+  {
+    document.getElementById('time').innerHTML = calcTime();
+  }
+
+  setTimeout(tick, 1);
+}
+
+function calcTime()
+{
+  var now = new Date();
+  var ms = now.getTime() - startTime.getTime();
+  var ms_ = Math.floor((ms % 1000) / 10);
+  var t = Math.floor(ms / 1000);
+  var s = t % 60;
+  t = Math.floor(t / 60);
+  var m = t % 60;
+  var h = Math.floor(t / 60); 
+ 
+  return formatTime(h, m, s, ms_);
+}
+
+function formatTime(h, m, s, ms)
+{
+  return formatNum(h) + ":" +
+         formatNum(m) + ":" + 
+         formatNum(s) + "." +
+         formatNum(ms);
+}
+
+function formatNum(n)
+{
+  return (n < 10) ? "0" + n : n;
+}
