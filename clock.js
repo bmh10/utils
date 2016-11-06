507ea128f6e72b6c690ae2758b7da535d78d31cf
bmh10 <bmhomer13@gmail.com>
2016-11-06 10:26:37 +0000

    Basic clock

---
+++ clock.js
@@ -0,0 +1,22 @@
+function updateTime()
+{
+  console.log('updating time');
+  var now = new Date();
+  var h = now.getHours()
+  var m = now.getMinutes();
+  var s = now.getSeconds();
+
+  document.getElementById('time').innerHTML = formatTime(h, m, s)
+  
+  setTimeout(updateTime, 1000);
+}
+
+function formatTime(h, m, s)
+{
+  return formatNum(h) + ":" + formatNum(m) + ":" + formatNum(s);
+}
+
+function formatNum(n)
+{
+  return (n < 10) ? "0" + n : n;
+}
