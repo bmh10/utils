65bccbe72cfa256a6fb1da742193733fbc803827
bmh10 <bmhomer13@gmail.com>
2016-11-13 17:41:01 +0000

    Calc basic button onclick

--- calculator.js
+++ calculator.js
@@ -1,3 +1,21 @@
-function updateTime()
+function init()
 {
+  var btns = document.getElementsByClassName('calc-btn');
+  for (var i = 0; i < btns.length; i++)
+  {
+    assignOnClick(btns, i);
+  }
+}
+
+function assignOnClick(btns, i)
+{
+  var param = btns[i].innerHTML;
+  console.log(param)
+  btns[i].onclick = function() { calc(param); }
+}
+
+function calc(n)
+{
+  console.log(n);
+  document.getElementById('solution').innerHTML = n;
 }
