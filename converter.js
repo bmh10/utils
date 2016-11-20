da906b85a33559263ac1962dc30cbfa1ddc554e1
bmh10 <bmhomer13@gmail.com>
2016-11-20 12:33:23 +0000

    Converter category selection

--- converter.js
+++ converter.js
@@ -1,4 +1,20 @@
 function init()
 {
   initDropdowns();
+
+  var categories = document.getElementsByClassName('category');
+  for (var i = 0; i < categories.length; i++)
+  {
+    assignOnClick(categories, i);
+  }
 }
+
+function assignOnClick(categories, i)
+{
+  var catName = categories[i].innerHTML; 
+  categories[i].onclick = function()
+  {
+    document.getElementById('selected-category').innerHTML = catName;
+  }
+}
+
