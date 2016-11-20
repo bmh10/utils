fbef7e45f353240c4d34e9615176ab9a1026f6a5
bmh10 <bmhomer13@gmail.com>
2016-11-20 15:09:10 +0000

    Init categories dropdown in js

--- converter.js
+++ converter.js
@@ -1,12 +1,16 @@
+
 function init()
 {
+  var categories = ["Length", "Time", "Volume", "Currency"];
   initDropdowns();
+  populateDropdown('category-dropdown', categories, 'category');
 
   var categories = document.getElementsByClassName('category');
   for (var i = 0; i < categories.length; i++)
   {
     assignOnClick(categories, i);
   }
+
 }
 
 function assignOnClick(categories, i)
@@ -18,3 +22,13 @@ function assignOnClick(categories, i)
   }
 }
 
+function populateDropdown(dropdownId, items, clazz)
+{
+  var dropdown = document.getElementById(dropdownId);
+  dropdown.innerHTML = "";
+  for (var i = 0; i < items.length; i++)
+  {
+    dropdown.innerHTML +=
+      '<li><a href="#" class="' + clazz  + '">' + items[i]  + '</a></li>';
+  }
+}
