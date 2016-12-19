477b204e0bee5524dd0910b6aee9c84c0e738d64
bmh10 <bmhomer13@gmail.com>
2016-12-19 12:04:35 +0000

    Convert after selecting unit from dropdown

--- converter.js
+++ converter.js
@@ -16,13 +16,17 @@ function init()
       initOnClick('unit', function(selected) {});
   });
 
-  initConversionListener('left-input', 'left-unit', 'right-input', 'right-unit');
-  initConversionListener('right-input', 'right-unit', 'left-input', 'left-unit');
+  // TODO refactor
+  initConversionListener('left-input', 'left-input', 'left-unit', 'right-input', 'right-unit');
+  initConversionListener('right-input', 'right-input', 'right-unit', 'left-input', 'left-unit');
+ 
+  initConversionListener('left-dropdown', 'left-input', 'left-unit', 'right-input', 'right-unit');
+  initConversionListener('right-dropdown', 'right-input', 'right-unit', 'left-input', 'left-unit');
 }
 
-function initConversionListener(srcInput, srcUnit, targetInput, targetUnit)
+function initConversionListener(listenerObj, srcInput, srcUnit, targetInput, targetUnit)
 {
-  var inputObj = document.getElementById(srcInput);
+  var inputObj = document.getElementById(listenerObj);
   inputObj.addEventListener('focusout', function() {
     var val = document.getElementById(srcInput).value;
     var unit = document.getElementById(srcUnit).innerHTML;
