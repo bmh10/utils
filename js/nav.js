88be54fdb1c0f4d4db2e7337a3dba451dc473855
bmh10 <bmhomer13@gmail.com>
2016-12-21 18:05:28 +0000

    Simplify home html

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,7 @@
 document.write(`<nav class="navbar navbar-default">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">Utils</a>
+      <a class="navbar-brand" href="home.html">Utils</a>
     </div>
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
