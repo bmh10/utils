82b02227e558ef3125efab70e842f6a58cd66e4f
bmh10 <bmhomer13@gmail.com>
2016-12-21 09:28:05 +0000

    Nav bar to each util

---
+++ nav.js
@@ -0,0 +1,16 @@
+document.write(`<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Utils</a>
+    </div>
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+	<li class="active"><a href="clock.html">Clock</a></li>
+	<li class="active"><a href="stopwatch.html">Stopwatch</a></li>
+	<li class="active"><a href="countdown.html">Timer</a></li>
+	<li class="active"><a href="calculator.html">Calculator</a></li>
+	<li class="active"><a href="converter.html">Converter</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>`);
