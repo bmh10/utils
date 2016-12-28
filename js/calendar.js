b7b5e454352293bef9db332b49aa734ade1148d3
bmh10 <bmhomer13@gmail.com>
2016-12-28 20:41:14 +0000

    First row of calendar

--- js/calendar.js
+++ js/calendar.js
@@ -1,11 +1,20 @@
 function loadCalendar()
 {
   var now = new Date();
-  var firstDayOfMonth = new Date(now.getYear(), now.getMonth(), 1);
-  var first
-  var lastDayOfMonth = new Date(now.getYear(), now.getMonth()+1, 0);
+  var firstDayOfMonth = new Date(now.getFullYear(), now.getMonth(), 1);
+  var lastDayOfMonth = new Date(now.getFullYear(), now.getMonth()+1, 0);
   document.getElementById('date').innerHTML = firstDayOfMonth + "-" + lastDayOfMonth;
 
+  console.log(firstDayOfMonth.getDay());
   var tr = document.getElementsByTagName('tr');
-  tr[1].innerHTML = '<tr>1</tr>'
+
+  // First row of calendar
+  var day = 1;
+  for (var i = 0; i < 7; i++) {
+    if (i < firstDayOfMonth.getDay()) {
+      tr[1].innerHTML += '<td></td>';
+    } else {
+      tr[1].innerHTML += '<td>' + (day++)  + '</td>'
+    }
+  }
 }
