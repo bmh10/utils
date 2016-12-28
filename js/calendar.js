be2fbf4b649a4d2db0c4081bc97c4277a17570ec
bmh10 <bmhomer13@gmail.com>
2016-12-28 20:22:41 +0000

    Start of table-based calendar

--- js/calendar.js
+++ js/calendar.js
@@ -1,4 +1,11 @@
 function loadCalendar()
 {
-  document.getElementById('date').innerHTML = new Date();
+  var now = new Date();
+  var firstDayOfMonth = new Date(now.getYear(), now.getMonth(), 1);
+  var first
+  var lastDayOfMonth = new Date(now.getYear(), now.getMonth()+1, 0);
+  document.getElementById('date').innerHTML = firstDayOfMonth + "-" + lastDayOfMonth;
+
+  var tr = document.getElementsByTagName('tr');
+  tr[1].innerHTML = '<tr>1</tr>'
 }
