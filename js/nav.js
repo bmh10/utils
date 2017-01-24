fac55e6a3a27213606b4c28a582c4299f450c257
bmh10 <bmhomer13@gmail.com>
2017-01-24 20:54:47 +0000

    Add feedback to nav

--- js/nav.js
+++ js/nav.js
@@ -13,6 +13,9 @@ document.write(`<nav class="navbar navbar-default">
 	<li class="active"><a href="calendar.html">Calendar</a></li>
 	<li class="active"><a href="notes.html">Notes</a></li>
       </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li class="active"><a href="feedback.html">Feedback</a></li>
+      </ul>
     </div>
   </div>
 </nav>`);
