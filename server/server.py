d3f2382c8ee4f6452bc89492c8c0a224615075ac
bmh10 <bmhomer13@gmail.com>
2017-03-20 20:36:07 +0000

    Add time to feedback log

--- server/server.py
+++ server/server.py
@@ -1,14 +1,16 @@
 from flask import Flask
 from flask import request
+import datetime
 app = Flask(__name__)
 
 @app.route("/feedback", methods=['POST'])
 def hello():
   typ = request.form['type']
   msg = request.form['message']
+  date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
   with open("feedback.log", "a") as feedbackFile:
-    feedbackFile.write(typ + " " + msg)
+    feedbackFile.write(date + " " + typ + " " + msg + "\n")
   return "Success"
 
 if __name__ == "__main__":
