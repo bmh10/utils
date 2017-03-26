b4d5d3040cff80e258837b2ad91d4e0de9694f6b
bmh10 <bmhomer13@gmail.com>
2017-03-26 17:25:49 +0100

    Working counter

--- js/counter.js
+++ js/counter.js
@@ -1,6 +1,41 @@
+var counter = 0;
 var counterElem;
+var incCounterBtn;
+var decCounterBtn;
 
 function init() {
   counterElem = document.getElementById('count');
-  counterElem.innerHTML = '0';
+  counterElem.innerHTML = counter;
+  incCounterBtn = document.getElementById('incCounter');
+  decCounterBtn = document.getElementById('decCounter');
+
+  hold(incCounterBtn, function() { adjCounter(1);  });
+  hold(decCounterBtn, function() { adjCounter(-1); });
+}
+
+function adjCounter(n) {
+  counter += n;
+  counterElem.innerHTML = counter; 
+}
+
+// TODO: same as in countdown.js - extract
+function hold(btn, action) {
+    var t;
+    var repeat = function () {
+        action();
+        t = setTimeout(repeat, start);
+        if (start > 100)
+        {
+          start = start * 0.6;
+        }
+    }
+
+    btn.onmousedown = function() {
+        start = 500;
+        repeat();
+    }
+
+    btn.onmouseup = function () {
+        clearTimeout(t);
+    }
 }
