af63f5f3238765d4f95829aec49886b72f4b4c50
bmh10 <bmhomer13@gmail.com>
2017-04-07 18:46:35 +0100

    View location on map

--- js/ip.js
+++ js/ip.js
@@ -3,15 +3,12 @@ function init() {
   document.getElementById('ip').innerHTML = info.ip;
   document.getElementById('location').innerHTML = 
     info.city + ', ' + info.region + ', ' + info.country;
-  
-
+  document.getElementById('map').href = 'https://www.google.co.uk/maps/place/' + info.loc;
 }
 
 function getIPInfo() {
-    xmlhttp = new XMLHttpRequest();
-    
-    xmlhttp.open("GET", "http://ipinfo.io/json", false);
-    xmlhttp.send();
-
-    return JSON.parse(xmlhttp.responseText);
+  xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET", "http://ipinfo.io/json", false);
+  xmlhttp.send();
+  return JSON.parse(xmlhttp.responseText);
 }
