a85b0e34fc7b7bcfcf6a187278adaa4a3b2609c7
bmh10 <bmhomer13@gmail.com>
2017-04-08 19:58:49 +0100

    Conversion listeners

--- js/currency.js
+++ js/currency.js
@@ -4,6 +4,14 @@ function init() {
   var currencies = Object.keys(info.rates);
   populateDropdown('unit-dropdown', currencies, 'unit');
   initOnClick('unit', function(selected) {})
+
+  
+  // TODO refactor - copied from converter.js
+  initConversionListener('left-input', 'left-input', 'left-unit', 'right-input', 'right-unit');
+  initConversionListener('right-input', 'right-input', 'right-unit', 'left-input', 'left-unit');
+ 
+  initConversionListener('left-dropdown', 'left-input', 'left-unit', 'right-input', 'right-unit');
+  initConversionListener('right-dropdown', 'left-input', 'left-unit', 'right-input', 'right-unit');
 }
 
 function getCurrencyInfo() {
@@ -13,3 +21,14 @@ function getCurrencyInfo() {
   return JSON.parse(xmlhttp.responseText);
 }
 
+function initConversionListener(listenerObj, srcInput, srcUnit, targetInput, targetUnit)
+{
+  var inputObj = document.getElementById(listenerObj);
+  inputObj.addEventListener('keyup', function() {
+    var val = document.getElementById(srcInput).value;
+    var unit = document.getElementById(srcUnit).innerHTML;
+    var tUnit = document.getElementById(targetUnit).innerHTML;
+    var tVal = 1; //convert(val, unit, tUnit, category);
+    document.getElementById(targetInput).value = tVal;
+   });
+}
