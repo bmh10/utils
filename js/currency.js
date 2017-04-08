2dc72d8ca3752561dc36415d1634ad9191999638
bmh10 <bmhomer13@gmail.com>
2017-04-08 18:20:21 +0100

    Populate currency dropdown

--- js/currency.js
+++ js/currency.js
@@ -1,6 +1,9 @@
 function init() {
+  initDropdowns();
   var info = getCurrencyInfo();
+  var currencies = Object.keys(info.rates);
   document.getElementById('info').innerHTML = JSON.stringify(info.rates);
+  populateDropdown('currency-dropdown', currencies, 'currency');
 }
 
 function getCurrencyInfo() {
@@ -9,3 +12,18 @@ function getCurrencyInfo() {
   xmlhttp.send();
   return JSON.parse(xmlhttp.responseText);
 }
+
+
+// TODO: duplicated from converter.js
+function populateDropdown(dropdownClass, items, clazz) {
+  var dropdowns = document.getElementsByClassName(dropdownClass);
+  for (var n = 0; n < dropdowns.length; n++) {   
+     var dropdown = dropdowns[n];
+     dropdown.innerHTML = "";
+     for (var i = 0; i < items.length; i++) {
+       dropdown.innerHTML +=
+         '<li><a href="#" class="' + clazz  + '">' + items[i]  + '</a></li>';        
+     }
+  }
+}
+
